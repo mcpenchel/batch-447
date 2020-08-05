662a39e7a84f577866d9d50d945fedd80ccd2f36
Matheus Penchel <matheus.penchel@gmail.com>
2020-08-05 09:11:23 -0300

    Finished the employee model

---
+++ livecode/food-delivery-reboot/app/models/employee.rb
@@ -0,0 +1,25 @@
+class Employee
+  # attr_reader :id, :username, :password, :role
+  def initialize(attributes = {})
+    @id = attributes[:id]
+    @username = attributes[:username]
+    @password = attributes[:password]
+    @role = attributes[:role]
+  end
+end
+
+# milene = Employee.new(id: 1, username: "milene.french",
+#                       password: "pretzel", role: "delivery_guy")
+
+# matheus = Employee.new(id: 2, username: "mastodon.matt",
+#                       password: "mastodon", role: "manager")
+
+# puts milene.id
+# puts milene.username
+# puts milene.password
+# puts milene.role
+
+# puts matheus.id
+# puts matheus.username
+# puts matheus.password
+# puts matheus.role
